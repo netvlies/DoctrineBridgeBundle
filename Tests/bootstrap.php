3f6a84c2aaabdc77e8b2afd285110078fd11efa8
M. de Krijger <mdekrijger@netvlies.nl>
2012-07-26 22:01:20 +0200

    Added initial test suite (not working yet)

---
+++ Tests/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+$file = './vendor/autoload.php';
+if (file_exists($file)) {
+    $autoload = require_once $file;
+} else {
+    throw new RuntimeException('Install dependencies to run test suite.');
+}
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+AnnotationRegistry::registerLoader(function($class) use ($autoload) {
+    $autoload->loadClass($class);
+    return class_exists($class, false);
+});
+AnnotationRegistry::registerFile(__DIR__.'/lib/Doctrine/ODM/PHPCR/Mapping/Annotations/DoctrineAnnotations.php');
+
+
+// tests are not autoloaded the composer.json
+$autoload->add('Tests', __DIR__);
